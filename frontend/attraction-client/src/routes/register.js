b95e1bbca5d4b58c874bc77888b0b427a8fb979a
Chefpeter <p.costadedoi@gmail.com>
2022-03-21 15:37:23 +0100

    .

--- frontend/attraction-client/src/routes/register.js
+++ frontend/attraction-client/src/routes/register.js
@@ -1,7 +1,7 @@
 import './App.css';
 import "./register.css";
 import { Link } from "react-router-dom";
-import { Button, TextField } from '@mui/material';
+import { Button, TextField, Alert, AlertTitle} from '@mui/material';
 import Header from '../components/header';
 import DeleteIcon from '@mui/icons-material/Delete';
 
@@ -9,6 +9,7 @@ function Login() {
   return (
     <div id='hintergrund'>
         <div id='eingaben'>
+
             <TextField id="filled-basic" label="Benutzername" variant="filled" />
             <br></br>
             <TextField id="filled-basic" label="Email" variant="filled" />
@@ -18,6 +19,13 @@ function Login() {
             <TextField id="filled-basic" label="Passwort Wiederholen" variant="filled" />
             <br></br>
             <Button variant="text">Registrieren</Button>
+           
+            <div>
+                <Alert severity="info">
+                <AlertTitle>Info</AlertTitle>
+                Eine Bestätigungsemail wurde versandt — <strong>Checke dein Postfach</strong>
+                </Alert>
+            </div>
         </div>    
     </div>
   );

fb50e8e12a643adbbf8bde9c2d827030d983982e
zbaakez <58550171+zbaakez@users.noreply.github.com>
2022-03-21 15:48:54 +0100

    update

--- frontend/attraction-client/src/routes/start.jsx
+++ frontend/attraction-client/src/routes/start.jsx
@@ -1,10 +1,21 @@
+import { ThemeProvider } from "@emotion/react";
 import { Link } from "react-router-dom";
+import Header from "../components/header"
+import "./start.css"
+
 
 export default function Start() {
+
     return (
-      <main style={{ padding: "1rem 0" }}>
+      
+      <main style={{ padding: "0 0" }}>
+     
+        <Header></Header>
         <h2>Du befindest dich auf der Startseite.</h2>
         <Link to="/">Home</Link>
+        <div id="test"></div>
+       
       </main>
+     
     );
   }

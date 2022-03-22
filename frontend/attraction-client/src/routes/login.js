446dda4bcdee8762d89af3245eadfaa8615951e6
Chefpeter <p.costadedoi@gmail.com>
2022-03-22 08:45:58 +0100

    login

--- frontend/attraction-client/src/routes/login.js
+++ frontend/attraction-client/src/routes/login.js
@@ -1,25 +1,24 @@
 import './App.css';
 import "./login.css";
 import { Link } from "react-router-dom";
-import { Button, TextField } from '@mui/material';
+import { Button, TextField, Typography } from '@mui/material';
 import Header from '../components/header';
 import DeleteIcon from '@mui/icons-material/Delete';
 
 function Login(props) {
   return (
-    <div id='hintergrund'>
-      <p>hallo </p>
-     
+    <div id='hintergrund'>     
       <div id='titel'>
         <h1>City2Go</h1>
       </div>
       <div id = "textfeld">
-        <TextField id="filled-basic username" label="Benutzername" variant="filled" />
+        <TextField id="filled-basic" label="Benutzername" variant="filled" />
         <br></br>
         <TextField id="filled-basic" label="Passwort" variant="filled" />
         <br></br>
-        <Button variant="text"><Link to="/home" style={{ textDecoration: 'none' }}>Anmelden</Link></Button>    
-        <Button variant="text"><Link to="/register" style={{ textDecoration: 'none' }}>Neuer Benutzer</Link></Button>
+        <Button id='knopf' variant="text"><Link to="/home" style={{ textDecoration: 'none' }}>Anmelden</Link></Button>    
+        <br></br>
+        <Button id='knopf' variant="text"><Link to="/register" style={{ textDecoration: 'none' }}>Neuer Benutzer</Link></Button>
       </div>
     </div>    
   );
--- frontend/attraction-client/src/routes/register.js
+++ frontend/attraction-client/src/routes/register.js
@@ -10,7 +10,7 @@ function Login() {
     <div id='hintergrund'>
         <div id='eingaben'>
 
-            <TextField id="filled-basic username" label="Benutzername" variant="filled" />
+            <TextField id="filled-basic" label="Benutzername" variant="filled" />
             <br></br>
             <TextField id="filled-basic" label="Email" variant="filled" />
             <br></br>
@@ -18,17 +18,16 @@ function Login() {
             <br></br>
             <TextField id="filled-basic" label="Passwort Wiederholen" variant="filled" />
             <br></br>
-            <Button variant="text" onClick={() => post()}>Registrieren</Button>
-           
+            <Button id = "knopf" variant="text" onClick={() => post()}>Registrieren</Button>
+            <br></br>
+            <Button id='knopf' variant="text"><Link to="/" style={{ textDecoration: 'none' }}>Schon angemeldet?</Link></Button>
+            <br></br>
             <div>
                 <Alert severity="info">
                 <AlertTitle>Info</AlertTitle>
-                Eine Bestätigungsemail wurde versandt — <strong>Checke dein Postfach</strong>
+                    Eine Bestätigungsemail wurde versandt — <strong>Checke dein Postfach</strong>
                 </Alert>
             </div>
-
-            <Button variant="text"><Link to="/" style={{ textDecoration: 'none' }}>Schon angemeldet?</Link></Button>
-
         </div>    
     </div>
   );

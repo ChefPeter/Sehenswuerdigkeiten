3797eb134d10f1ffac733bd178e0bdc603db9670
Chefpeter <p.costadedoi@gmail.com>
2022-03-23 08:48:12 +0100

    light and dark mode

--- frontend/attraction-client/src/components/BaseMap.jsx
+++ frontend/attraction-client/src/components/BaseMap.jsx
@@ -1,22 +1,42 @@
 import React, { useEffect } from "react";
 import mapboxgl from "mapbox-gl";
 import "./BaseMap.scss";
+import { useSelector } from 'react-redux';
+
+
+
 
 const BaseMap = () => {
+
+    const themeN = useSelector(state => {
+        try{
+           
+            if(state.theme == "light"){
+                return "mapbox://styles/mapbox/light-v10"
+            }else{
+                return "mapbox://styles/mapbox/navigation-night-v1"
+            }
+          
+        }catch(e){
+          return "mapbox://styles/mapbox/navigation-night-v1";
+        }
+      });
+      const language = useSelector(state => {
+        try{
+          return state.language;
+        }catch(e){
+          return "de";
+        }
+      });
+    
+
   mapboxgl.accessToken = "pk.eyJ1IjoiemJhYWtleiIsImEiOiJja3pvaXJ3eWM0bnV2MnVvMTc2d2U5aTNpIn0.RY-K9qwZD1hseyM5TxLzww";
 
   useEffect(() => {
-    let theme;
-    let i = 1;
-    if(i == 0){
-        theme = "mapbox://styles/mapbox/light-v10";
-    }
-    else{
-        theme = "mapbox://styles/mapbox/navigation-night-v1"
-    }
+    
     const map = new mapboxgl.Map({
       container: "mapContainer",
-      style: theme,
+      style: themeN,
       center: [-74.5, 40],
       zoom: 9,
     });

b25f0072de7f4f8d519e3348122dbd423a285410
Git_Goat <74654447+noapichler@users.noreply.github.com>
2022-03-23 08:51:27 +0100

    .

--- frontend/attraction-client/src/components/BaseMap.jsx
+++ frontend/attraction-client/src/components/BaseMap.jsx
@@ -1,9 +1,15 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import mapboxgl from "mapbox-gl";
+import fetchFakeData from "./fetchFakeData";
+import Popup from "./Popup";
 import "./BaseMap.scss";
+import ReactDOM from "react-dom";
 
 const BaseMap = () => {
   mapboxgl.accessToken = "pk.eyJ1IjoiemJhYWtleiIsImEiOiJja3pvaXJ3eWM0bnV2MnVvMTc2d2U5aTNpIn0.RY-K9qwZD1hseyM5TxLzww";
+
+  const mapContainerRef = useRef(null);
+  const popUpRef = useRef(new mapboxgl.Popup({ offset: 15 }));
 
   useEffect(() => {
     let theme;
@@ -30,10 +36,75 @@ const BaseMap = () => {
         trackUserLocation: true
       });
       
-      map.addControl(geolocate, "top-right")
+      map.addControl(geolocate, "top-right");
+
+      map.on("load", () => {
+        // add the data source for new a feature collection with no features
+        map.addSource("random-points-data", {
+          type: "geojson",
+          data: {
+            type: "FeatureCollection",
+            features: []
+          }
+        });
+        // now add the layer, and reference the data source above by name
+        map.addLayer({
+          id: "random-points-layer",
+          source: "random-points-data",
+          type: "symbol",
+          layout: {
+            // full list of icons here: https://labs.mapbox.com/maki-icons
+            "icon-image": "bakery-15", // this will put little croissants on our map
+            "icon-padding": 0,
+            "icon-allow-overlap": true
+          }
+        });
+      });
+
+      
+    map.on("moveend", async () => {
+      // get new center coordinates
+      const { lng, lat } = map.getCenter();
+      // fetch new data
+      const results = await fetchFakeData({ longitude: lng, latitude: lat });
+      // update "random-points-data" source with new data
+      // all layers that consume the "random-points-data" data source will be updated automatically
+      map.getSource("random-points-data").setData(results);
+    });
+
+    // change cursor to pointer when user hovers over a clickable feature
+    map.on("mouseenter", "random-points-layer", e => {
+      if (e.features.length) {
+        map.getCanvas().style.cursor = "pointer";
+      }
+    });
+
+    // reset cursor to default when user is no longer hovering over a clickable feature
+    map.on("mouseleave", "random-points-layer", () => {
+      map.getCanvas().style.cursor = "";
+    });
+
+    // add popup when user clicks a point
+    map.on("click", "random-points-layer", e => {
+      if (e.features.length) {
+        const feature = e.features[0];
+        // create popup node
+        const popupNode = document.createElement("div");
+        ReactDOM.render(<Popup feature={feature} />, popupNode);
+        // set popup on map
+        popUpRef.current
+          .setLngLat(feature.geometry.coordinates)
+          .setDOMContent(popupNode)
+          .addTo(map);
+      }
+    });
+
+    // clean up on unmount
+    return () => map.remove();
+      
   }, []);
   
-  return <div id="mapContainer" className="map"></div>;
+  return <div id="mapContainer" className="map" ref={mapContainerRef}></div>;
 };
 
 export default BaseMap;

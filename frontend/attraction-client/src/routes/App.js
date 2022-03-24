904069fc02ecc3afc99a7e2e33ee42b7218820db
Git_Goat <74654447+noapichler@users.noreply.github.com>
2022-03-24 09:36:46 +0100

    api daten sehenswürdigkeiten

--- frontend/attraction-client/src/routes/App.js
+++ frontend/attraction-client/src/routes/App.js
@@ -42,28 +42,41 @@ function App(props) {
     }
   });
 
-  async function getDataFromAPI(url)
-  {
-    let result = await fetch(url);
-    let answer = null;
-    if(result.ok)
-      answer = await result.json();
-    return answer;
+  const apiKey = "5ae2e3f221c38a28845f05b690c520033dc6de71c6665213ffad8752";
+
+  function apiGet(method, query) {
+    return new Promise(function(resolve, reject) {
+      var otmAPI =
+        "https://api.opentripmap.com/0.1/en/places/" +
+        method +
+        "?apikey=" +
+        apiKey;
+      if (query !== undefined) {
+        otmAPI += "&" + query;
+        console.log("a  a")
+      }
+      fetch(otmAPI)
+        .then(response => response.json())
+        .then(data => console.log(data))
+        .catch(function(err) {
+          console.log("Fetch Error :-S", err);
+        });
+    });
   }
 
-  async function enter()
+  async function getDataFromAPI()
   {
-      const URL = "https://heritage.toolforge.org/api/api.php?action=search&srcountry=fr&srlang=fr&srmunicipality=[[Aix-en-Provence]]&format=json";
-      let result = getDataFromAPI(URL);
-      console.log(result);
+    
+    apiGet(
+      "radius",
+      `radius=10000&limit=${100}&offset=${0}&lon=${11.6615276}&lat=${46.7217851}&rate=2&format=json`
+    )
   }
-
-
   
     return (
       <ThemeProvider theme={createTheme(theme === "dark" ? dark : light)}>
       <Header/>
-      <Button onClick={() => enter()}>click me for data</Button>
+      <Button onClick={() => getDataFromAPI()}>click me for data</Button>
       <BaseMap />
       <Slider></Slider>
       <a>index</a>

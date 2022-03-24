fc09dd89c102c33daf7156f7e449c36366629e4a
Chefpeter <p.costadedoi@gmail.com>
2022-03-24 08:54:24 +0100

    files für olli

---
+++ frontend/attraction-client/src/routes/profile.jsx
@@ -0,0 +1,48 @@
+import TextareaAutosize from '@mui/material/TextareaAutosize';
+import { useSelector } from 'react-redux';
+import "../routes/styles/contact.css";
+import Header from "../components/header";
+import React from "react";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { Button} from '@mui/material';
+
+// Define theme settings
+const light = {
+  palette: {
+    mode: "light",
+  },
+};
+
+const dark = {
+  palette: {
+    mode: "dark",
+  },
+};
+
+function Contact(props) {
+
+  const theme = useSelector(state => {
+    try{
+      return state.theme;
+    }catch(e){
+      return "dark";
+    }
+  });
+  const language = useSelector(state => {
+    try{
+      return state.language;
+    }catch(e){
+      return "de";
+    }
+  });
+
+    return (
+      <div>
+        <ThemeProvider theme={createTheme(theme === "dark" ? dark : light)}>
+        <Header/>
+        </ThemeProvider>
+      </div>
+    );
+  
+}
+export default Contact;

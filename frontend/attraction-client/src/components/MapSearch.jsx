53cb2fcabf6bbf5dd0ce7575b2dbae2f22959aff
zbaakez <58550171+zbaakez@users.noreply.github.com>
2022-03-28 13:02:41 +0200

    implemented autocomplete [wip]

--- frontend/attraction-client/src/components/MapSearch.jsx
+++ frontend/attraction-client/src/components/MapSearch.jsx
@@ -1,32 +1,71 @@
 import React from "react";
-import { Paper, Button, TextField, Container, Card, Box, Divider } from "@mui/material";
+import { Paper, Button, TextField, Container, Card, Box, Divider, Stack, Autocomplete } from "@mui/material";
 import "./styles/mapsearch.css";
 import TravelExploreIcon from '@mui/icons-material/TravelExplore';
 import GpsFixedIcon from '@mui/icons-material/GpsFixed';
 import { fontSize } from "@mui/system";
+import {useState , setState} from "react";
 
 let locationInput = "";
 
+//let locations = [{name:"Sage"},{name:"Tage"}]
+
 function MapSearch () {
 
-    const handleSearchFriendInput = (event)=>{
+    const [locations, setLocations] = useState([{name:"Sage"},{name:"Tage"}])
+
+    const handleSearchFriendInput = async (event)=>{
+
+        let locations2 = [];
+        console.log("hallo")
+        let jsonRes = "";
+
+        if(event.target.value.length > 2){
+
+            let result = await fetch("https://api.mapbox.com/geocoding/v5/mapbox.places/"+new URLSearchParams(event.target.value).toString().slice(0,-1)+".json?limit=10&proximity=ip&types=place%2Cpostcode%2Caddress%2Clocality%2Cneighborhood%2Cdistrict&language=de&access_token=pk.eyJ1IjoiemJhYWtleiIsImEiOiJja3pvaXJ3eWM0bnV2MnVvMTc2d2U5aTNpIn0.RY-K9qwZD1hseyM5TxLzww", {
+                method: "GET"
+            });
+
+            jsonRes = await result.json();
+            console.log(jsonRes["features"][0])
+        }
+
+        // 0 -10
+        setLocations([{name:JSON.stringify(jsonRes["features"][0]["place_name"])},{name:JSON.stringify(jsonRes["features"][0]["place_name"])}])
+       
         locationInput = event.target.value;
+
     };
 
+ 
     return (
-        <Paper id="searchPaper" elevation={10} >
-            <TextField
-                fullWidth
-                id="searchBarLocation"
-                type="text"
-                label="Search a place"
-                
-                onChange={handleSearchFriendInput}
-                InputProps={{endAdornment: <Container style={{display:"flex", marginRight:"1.2vw", maxWidth:"130px"}}><Button><TravelExploreIcon onClick={() => console.log("explore")} /> </Button>  <Divider orientation="vertical" flexItem /> <Button><GpsFixedIcon  onClick={() => console.log("gps")} /></Button></Container> }}
-            />
-           
 
+        <Paper id="searchPaper" elevation={10} >
+          
+            <div>{locations[0].name}</div>
+            <Autocomplete
+                freeSolo
+                id="idAutocomplete"
+                fullWidth
+                disableClearable
+                
+                options={locations.map((option) => option.name)}
+                renderInput={(params) => (
+                <TextField
+                    onChange={handleSearchFriendInput}
+                    {...params}
+                    label="Search a place"
+                    InputProps={{
+                    ...params.InputProps,
+                    
+                    endAdornment: <Container style={{display:"flex", marginRight:"1.2vw", maxWidth:"130px"}}><Button><TravelExploreIcon onClick={() => console.log("explore")} /> </Button>  <Divider orientation="vertical" flexItem /> <Button><GpsFixedIcon  onClick={() => console.log("gps")} /></Button></Container> 
+                    }}
+                />
+                )}
+            />
+  
         </Paper>
+
     );
 
 
--- frontend/attraction-client/src/routes/login.jsx
+++ frontend/attraction-client/src/routes/login.jsx
@@ -8,45 +8,47 @@
     let passwordInput = "";
 
     function Login(props) {
-    const navigate = useNavigate();
-    const getUsernameValue = (event)=>{
-        usernameInput = event.target.value;;
-    };
-    const getPasswordValue = (event) => {
-        passwordInput = event.target.value;
-    };
 
-    const [errorText, setErrorText] = useState("Error");
-    const [showErrorAlert, setShowErrorAlert] = useState(false);
+        const navigate = useNavigate();
+        const getUsernameValue = (event)=>{
+            usernameInput = event.target.value;;
+        };
+        const getPasswordValue = (event) => {
+            passwordInput = event.target.value;
+        };
 
-    return (
-        <div id='hintergrund'>     
-        <div id='titel'>
-            <h1>City2Go</h1>
-        </div>
-        <div id = "textfeld">
-            <div id="widthTextfields">
-            <div id="textFieldsLogin">
-                <TextField sx={{ marginBottom: 1 }} fullWidth id="filled-basic" label="Benutzername" variant="filled" onChange={getUsernameValue} />
-                <TextField sx={{ marginBottom: 0.5 }} fullWidth id="filled-password-input" label="Passwort" type="password" autoComplete="current-password" variant="filled" onChange={getPasswordValue} />
+        const [errorText, setErrorText] = useState("Error");
+        const [showErrorAlert, setShowErrorAlert] = useState(false);
+
+        return (
+            <div id='hintergrund'>     
+            <div id='titel'>
+                <h1>City2Go</h1>
             </div>
-            <Button fullWidth id='btnLoginPage' variant="conained" onClick={ () => post(setErrorText, setShowErrorAlert)}>Anmelden!</Button>    
-            <Button fullWidth id='btnLoginPage' variant="conained" onClick={() => navigate("/register")}>Neuer Benutzer</Button>
-            <Button fullWidth id='btnLoginPage' variant="conained" onClick={() => navigate("/requestreset")}>Passwort zurücksetzen</Button>
-            {showErrorAlert ?
-                <Fade in={showErrorAlert} timeout={250}>
-                <Alert id="loginErrorAlert" severity="error"> 
-                    <AlertTitle>Error</AlertTitle>
-                        {errorText}
-                </Alert>
-                </Fade>
-            : null}
+            <div id = "textfeld">
+                <div id="widthTextfields">
+                <div id="textFieldsLogin">
+                    <TextField sx={{ marginBottom: 1 }} fullWidth id="filled-basic" label="Benutzername" variant="filled" onChange={getUsernameValue} />
+                    <TextField sx={{ marginBottom: 0.5 }} fullWidth id="filled-password-input" label="Passwort" type="password" autoComplete="current-password" variant="filled" onChange={getPasswordValue} />
+                </div>
+                <Button fullWidth id='btnLoginPage' variant="conained" onClick={ () => post(setErrorText, setShowErrorAlert)}>Anmelden!</Button>    
+                <Button fullWidth id='btnLoginPage' variant="conained" onClick={() => navigate("/register")}>Neuer Benutzer</Button>
+                <Button fullWidth id='btnLoginPage' variant="conained" onClick={() => navigate("/requestreset")}>Passwort zurücksetzen</Button>
+                {showErrorAlert ?
+                    <Fade in={showErrorAlert} timeout={250}>
+                    <Alert id="loginErrorAlert" severity="error"> 
+                        <AlertTitle>Error</AlertTitle>
+                            {errorText}
+                    </Alert>
+                    </Fade>
+                : null}
 
+                </div>
             </div>
-        </div>
+            
+            </div>   
+        );
         
-        </div>   
-    );
     }
 
     function post (setErrorText, setShowErrorAlert){

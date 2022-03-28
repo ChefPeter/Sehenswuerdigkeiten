f833d2b1eed01d8fcb3ff80a2d3fb1c111347b75
Lukas1803 <lukasgallmetzer1@gmail.com>
2022-03-29 01:45:39 +0200

    Update BaseMap.jsx

--- frontend/attraction-client/src/components/BaseMap.jsx
+++ frontend/attraction-client/src/components/BaseMap.jsx
@@ -265,10 +265,12 @@ const BaseMap = () => {
         onClose={() => setOpen(false)}
         onOpen={() => {}}>
           <div>
-            <img src="https://media.istockphoto.com/photos/colosseum-in-rome-during-sunrise-picture-id1271579758?b=1&k=20&m=1271579758&s=170667a&w=0&h=oyXB8ehFjbo5-9HDdSjI9hYZktLstV3Ixz4JUUynahU=" style={{width:"100%", height:"12%"}}></img>
+            <div style={{width:"100%", maxHeight:"30%", maxWidth:"100%"}}>
+            <div><img src="https://media.istockphoto.com/photos/colosseum-in-rome-during-sunrise-picture-id1271579758?b=1&k=20&m=1271579758&s=170667a&w=0&h=oyXB8ehFjbo5-9HDdSjI9hYZktLstV3Ixz4JUUynahU=" style={{maxWidth:"100%"}}></img></div>
             <div style={{display: "flex", justifyContent: "space-between"}}>
               <h2 >Name</h2>
               <Button variant="contained" style={{borderRadius: '20px', backgroundColor: "white", color: "black"}}>Add</Button>
+            </div>
             </div>
           </div>
             

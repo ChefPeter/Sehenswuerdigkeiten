bb8295953902e4b3be86d8b9b0dff66a549315fd
Git_Goat <74654447+noapichler@users.noreply.github.com>
2022-03-29 10:31:11 +0200

    .

--- frontend/attraction-client/src/components/BaseMap.jsx
+++ frontend/attraction-client/src/components/BaseMap.jsx
@@ -209,10 +209,18 @@ const BaseMap = () => {
     });
 
     // change cursor to pointer when user hovers over a clickable feature
-    map.on("mouseenter", "random-points-layer", e => {
+    map.on("mouseenter", "random-points-layer", async e => {
 
       if (e.features.length) {
         const feature = e.features[0];
+        console.log(feature.properties.id)
+        let desc=await getDetail(feature.properties.id)
+        let pdesc="";
+        console.log("aa")
+        console.log(desc)
+        /*if(desc.hasOwnProperty('wikipedia_extracts')){
+          pdesc=desc.wikipedia_extracts.text
+        }*/
         // create popup node
         const popupNode = document.createElement("div");
 
@@ -220,7 +228,7 @@ const BaseMap = () => {
         // <img src="./kolosseum.jpg"></img>
         //ReactDOM.render(<Button variant="contained" style={{borderRadius: '20px'}} onClick={() => addToRoute(feature)}>Add</Button>, popupNode);
         ReactDOM.render(<div><img src="https://media.istockphoto.com/photos/colosseum-in-rome-during-sunrise-picture-id1271579758?b=1&k=20&m=1271579758&s=170667a&w=0&h=oyXB8ehFjbo5-9HDdSjI9hYZktLstV3Ixz4JUUynahU=" style={{width:"100%", height:"50%"}}></img>
-          <div style={{display: "flex", justifyContent: "space-between"}}><h2 >Name</h2>
+          <div style={{display: "flex", justifyContent: "space-between"}}><p>{desc.name}</p>
           <Button variant="contained" style={{borderRadius: '20px', backgroundColor: "white", color: "black"}} onClick={() => addToRoute(feature)}>Add</Button></div></div>, popupNode);
           
         if(testRoute.length > 3)
@@ -235,6 +243,14 @@ const BaseMap = () => {
                
       }
     });
+
+    async function getDetail(id){
+      let result = await fetch('https://api.opentripmap.com/0.1/en/places/xid/'+id+'?apikey=5ae2e3f221c38a28845f05b690c520033dc6de71c6665213ffad8752');
+      let answer = null;
+      if(result.ok)
+        answer = await result.json();
+      return answer;
+    }
 
     // reset cursor to default when user is no longer hovering over a clickable feature
     map.on("mouseleave", "random-points-layer", () => {

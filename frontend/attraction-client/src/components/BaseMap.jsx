9d6ab796dbd1ce954d911a7faafefcb726e032ee
obenat <stobenat@bx.fallmerayer.it>
2022-03-29 20:30:44 +0200

    added Images

--- frontend/attraction-client/src/components/BaseMap.jsx
+++ frontend/attraction-client/src/components/BaseMap.jsx
@@ -54,7 +54,7 @@ export async function postRoute()
       map.removeLayer("route1");
       map.removeSource('route1');
       map.removeLayer("route2");
-      map.removeSource('route2');
+      map.removeSource('route');
     }
     for(let count = 1; count < coords.length; count++)
     {
@@ -256,12 +256,15 @@ const BaseMap = () => {
     map.on("mouseenter", "random-points-layer", async e => {
 
       if (e.features.length) {
-        const feature = e.features[0];
-        console.log(feature.properties.id)
+        const feature = e.features[0];   
+
+        /*console.log(feature.properties.id)
+        console.log("asdfasdfasdfasdf");
+        console.log(feature.properties.wikidata);
         let desc=await getDetail(feature.properties.id)
         let pdesc="";
         console.log("aa")
-        console.log(desc)
+        console.log(desc)*/
         /*if(desc.hasOwnProperty('wikipedia_extracts')){
           pdesc=desc.wikipedia_extracts.text
         }*/
@@ -271,9 +274,9 @@ const BaseMap = () => {
         ReactDOM.render(<Popup feature={feature} />, popupNode);
         // <img src="./kolosseum.jpg"></img>
         //ReactDOM.render(<Button variant="contained" style={{borderRadius: '20px'}} onClick={() => addToRoute(feature)}>Add</Button>, popupNode);
-        ReactDOM.render(<div><img src="https://media.istockphoto.com/photos/colosseum-in-rome-during-sunrise-picture-id1271579758?b=1&k=20&m=1271579758&s=170667a&w=0&h=oyXB8ehFjbo5-9HDdSjI9hYZktLstV3Ixz4JUUynahU=" style={{width:"100%", height:"50%"}}></img>
-          <div style={{display: "flex", justifyContent: "space-between"}}><p style={{color:'black'}}>{desc.name}</p>
-          <Button variant="contained" style={{borderRadius: '20px', backgroundColor: "white", color: "black"}} onClick={() => addToRoute(e.features[0])}>Add</Button></div></div>, popupNode);
+        ReactDOM.render(<div><img src={`https://commons.wikimedia.org/wiki/Special:FilePath/${(await getDataFromURL(`https://www.wikidata.org/w/api.php?action=wbgetclaims&property=P18&entity=${feature.properties.wikidata}&format=json&origin=*`)).claims.P18[0].mainsnak.datavalue.value.replace(/\s/g, "_")}?width=300`} style={{width:"100%", height:"50%"}}></img>
+          <div style={{display: "flex", justifyContent: "space-between"}}><p style={{color:'black'}}>{feature.properties.name}</p>
+          <Button variant="contained" style={{borderRadius: '20px', backgroundColor: "white", color: "black"}} onClick={() => addToRoute(feature)}>Add</Button></div></div>, popupNode);
 
         //popupNode  = document.createElement("div");
         // set popup on map
--- frontend/attraction-client/src/components/fetchFakeData.js
+++ frontend/attraction-client/src/components/fetchFakeData.js
@@ -1,47 +1,47 @@
-
- const fetchFakeData = async centerCoordinates => {
-    
-    const API_KEY = "5ae2e3f221c38a28845f05b690c520033dc6de71c6665213ffad8752";
- 
-    let points = []; 
-    let results = await getDataFromURL(getURL("radius", 1000, centerCoordinates.latitude, centerCoordinates.longitude, 100));
-    for(let result of results)
-    {
-        const lon = result.point.lon;
-        const lat = result.point.lat;
-        points.push({
-          type: "Feature",
-          geometry: {
-            type: "Point",
-            coordinates: [lon, lat],
-          },
-          properties: {
-            id: result.xid,
-            name: result.name,
-            title: "",
-            description: "",
-            img: ""     
-          }
-        });   
-    }
-  function getURL(method, radius, lat, lon, limit)
-  {
-    return `https://api.opentripmap.com/0.1/en/places/${method}?apikey=${API_KEY}&radius=${radius}&limit=${limit}&offset=0&lon=${lon}&lat=${lat}&rate=2&format=json`;
+const fetchFakeData = async centerCoordinates => {
+  async function getDataFromURL(url) {
+      let result = await fetch(url);
+      let answer = null;
+      if (result.ok)
+          answer = await result.json();
+      return answer;
   }
 
-  async function getDataFromURL(url)
+  function getURL(radius, lat, lon, limit, fame = "3h") {
+      return `https://api.opentripmap.com/0.1/en/places/radius?radius=${radius}&lon=${lon}&lat=${lat}&rate=${fame}&format=json&limit=${limit}&apikey=${API_KEY}`;
+      //Old URL
+      //return `https://api.opentripmap.com/0.1/en/places/${method}?apikey=${API_KEY}&radius=${radius}&limit=${limit}&offset=0&lon=${lon}&lat=${lat}&rate=2&format=json`;
+  }
+
+  const API_KEY = "5ae2e3f221c38a28845f05b690c520033dc6de71c6665213ffad8752";
+
+  let points = [];
+  let results = await getDataFromURL(getURL(1000, centerCoordinates.latitude, centerCoordinates.longitude, 100));
+
+  for (let result of results)
   {
-    let result = await fetch(url);
-    let answer = null;
-    if(result.ok)
-      answer = await result.json();
-    return answer;
+      if(results.some(x => x.id != result.id && x.wikidata === result.wikidata))
+        continue;
+      const lon = result.point.lon;
+      const lat = result.point.lat;
+      points.push({
+          type: "Feature",
+          geometry: {
+              type: "Point",
+              coordinates: [lon, lat],
+          },
+          properties: {
+              id: result.xid,
+              name: result.name,
+              wikidata: result.wikidata,
+          }
+      });
   }
 
   return Promise.resolve({
-    type: "FeatureCollection",
-    features: points,
+      type: "FeatureCollection",
+      features: points,
   });
 
 };
-  export default fetchFakeData;
+export default fetchFakeData;

7de5de49aaef5c7c9eda8b92f30e05e10686749a
zLuki <61950131+zLuki@users.noreply.github.com>
2022-04-01 08:41:37 +0200

    It is now possible to add live position :thumbsup:

---
+++ backend/src/get-position.js
@@ -0,0 +1,26 @@
+const mysql = require("mysql");
+const util = require("util");
+
+async function getPosition(request) {
+    let conn;
+    try {
+        conn = mysql.createConnection({
+            host: process.env.DB_HOST,
+            user: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_DATABASE
+        });
+        const query = util.promisify(conn.query).bind(conn);
+        return Array.from(await query(
+            `SELECT latitude, longtitude FROM users
+                WHERE username='${request.session.username}'`
+        ));
+    } catch(e) {
+        console.error(e);
+        return "Fehler mit der Datenbank!";
+    } finally {
+        conn.end();
+    }
+}
+
+module.exports = getPosition;
--- backend/src/server.js
+++ backend/src/server.js
@@ -15,6 +15,7 @@ const leaveGroup = require("./leave-group");
 const addToGroup = require("./add-to-group");
 const sendSights = require("./send-sights");
 const addSight = require("./add-sight");
+const addPosition = require("./add-position");
 
 const getDescription = require("./get-description");
 const getFriends = require("./get-friends");
@@ -26,6 +27,7 @@ const getBestRoute = require("./get-best-route");
 const getUsers = require("./get-users");
 const getGroups = require("./get-groups");
 const getGroupConversation = require("./get-group-conversation");
+const getPosition = require("./get-position");
 
 const express = require("express");
 const session = require("express-session");
@@ -86,6 +88,7 @@ app.post("/leave-group", isAuthenticated, async(req, res) => sendResponse(await 
 app.post("/add-to-group", isAuthenticated, async(req, res) => sendResponse(await addToGroup(req), res));
 app.post("/sights", /*isAuthenticated,*/ async(req, res) => await sendSights(req, res));
 app.post("/add-sight", isAuthenticated, async(req, res) => sendResponse(await addSight(req), res));
+app.post("/add-position", isAuthenticated, async(req, res) => sendResponse(await addPosition(req), res));
 
 // GET REQUESTS
 app.get("/description", isAuthenticated, async(req, res) => sendGetResponse(await getDescription(req), res));
@@ -99,6 +102,7 @@ app.get("/file", (req, res) => getFile(req, res));
 app.get("/username", isAuthenticated, (req, res) => res.status(200).send(req.session.username));
 app.get("/groups", isAuthenticated, async(req, res) => sendGetResponse(await getGroups(req), res));
 app.get("/group-conversation", isAuthenticated, async(req, res) => sendGetResponse(await getGroupConversation(req), res));
+app.get("/position", isAuthenticated, async(req, res) => sendGetResponse(await getPosition(req), res));
 
 //app.get("/logged-in", (req, res) => res.send(req.session));
 

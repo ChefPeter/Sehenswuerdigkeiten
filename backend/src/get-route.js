9dc6a775497e69646871fbe92c4c1d34f82d0d67
zLuki <61950131+zLuki@users.noreply.github.com>
2022-04-08 12:31:13 +0200

    Get points of a route

---
+++ backend/src/get-route.js
@@ -0,0 +1,27 @@
+const mysql = require("mysql");
+const util = require("util");
+
+async function getRoute(request) {
+    let conn;
+    try {
+        conn = mysql.createConnection({
+            host: process.env.DB_HOST,
+            user: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_DATABASE
+        });
+        const query = util.promisify(conn.query).bind(conn);
+        return Array.from(await query(
+            `SELECT p.point_id, p.latitude, p.longtitude, p.name, p.wikidata FROM routes AS r
+                JOIN points AS p ON r.point_id = p.point_id
+                WHERE r.username='${request.session.username}' AND route_name='${request.query.route_name}'`
+        ));
+    } catch(e) {
+        console.error(e);
+        return null;
+    } finally {
+        conn.end();
+    }
+}
+
+module.exports = getRoute;
--- backend/src/server.js
+++ backend/src/server.js
@@ -38,7 +38,7 @@ const getAllPositions = require("./get-all-positions");
 const getVisibility = require("./get-visibility");
 const getGroupMembers = require("./get-group-members");
 const getRouteNames = require("./get-route-names");
-
+const getRoute = require("./get-route");
 
 const express = require("express");
 const session = require("express-session");
@@ -126,6 +126,7 @@ app.get("/all-positions", isAuthenticated, async(req, res) => sendGetResponse(aw
 app.get("/visibility", isAuthenticated, async(req, res) => sendGetResponse(await getVisibility(req), res));
 app.get("/group-members", isAuthenticated, async(req, res) => sendGetResponse(await getGroupMembers(req), res));
 app.get("/route-names", isAuthenticated, async(req, res) => sendGetResponse(await getRouteNames(req), res));
+app.get("/route", isAuthenticated, async(req, res) => sendGetResponse(await getRoute(req), res));
 //app.get("/logged-in", (req, res) => res.send(req.session));
 
 app.get("/", isAuthenticated, async(req, res) => {
